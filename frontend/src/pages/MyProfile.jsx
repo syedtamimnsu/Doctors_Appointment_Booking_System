d0dff15403cf16d847441a66c40944c400c13185
syed-tamim <syed.tamim@northsouth.edu>
2025-04-07 17:09:38 +0600

    project added

--- frontend/src/pages/MyProfile.jsx
+++ frontend/src/pages/MyProfile.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { assets } from '../assets/assets_frontend/assets';
 
 const MyProfile = () => {
+
+  const [userData, setUserData] = useState({
+    name: "Edward Vincent",
+    image: assets.profile_pic,
+    email: 'richardjameswap@gmail.com',
+    phone: '+1  123 456 7890',
+    address:{
+      line1: "57th Cross, Richmond ",
+      line2: "Circle, Church Road, London"
+    },
+    gender: 'Male',
+    dob: '2000-01-20'
+  })
+
+  const [isEdit, setIsEdit] = useState(false)
+
+
   return (
     <div>
+      <img src={userData.image} alt="" />
+
+      {
+        isEdit
+        ? <input type="text" value={userData.name} onChange={e=> setUserData(prev => ({...prev,name:e.target.value}))} />
+        : <p>{userData.name}</p>
+      }
       
     </div>
   )
