85d5ba10a37fcf7ba6f91c3840e9b39e08971a56
syed-tamim <syed.tamim@northsouth.edu>
2025-04-12 23:59:06 +0600

    project added

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 import cors from 'cors';
 import 'dotenv/config';
 import express from 'express';
+import connectDB from './config/mongodb.js';
 
 //app config
 const app = express()
 const port = process.env.PORT || 4000
+connectDB()
 
 
 //middleware
@@ -14,7 +16,7 @@ app.use(cors())
 
 //api end point
 app.get('/', (req , res )=>{
-    res.send('API working  Great')
+    res.send('API working   Great')
 })
 
 app.listen(port, ()=>console.log("server started", port))
